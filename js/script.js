7bb1e9cc62526c1239f538309eae005631602973
Justyna Sadowska <korzyniewskaj@gmail.com>
2021-09-12 21:02:42 +0200

    javascript-prework

--- js/script.js
+++ js/script.js
@@ -1,12 +1,35 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+if(randomNumber == 1){
+  computerMove = 'papier';
+}
+if(randomNumber == 3){
+  computerMove = 'nożyce';
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-let playerMove = `papier`;
+printMessage('Mój ruch to: ' + computerMove);
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+if(playerInput == '2'){
+  playerMove = 'papier';
+}
+if(playerInput == '1'){
+  playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
